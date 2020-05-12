26c33481ca30719d354a39fe68534ba85804d14b
Amrendra Pratap singh <amrendra.singh@aimeo.co>
2020-05-12 22:00:47 +0530

    Initial project setup

---
+++ SukshiAssignment/SukshiAssignment.h
@@ -0,0 +1,22 @@
+//
+//  SukshiAssignment.h
+//  SukshiAssignment
+//
+//  Created by Zoheb Ahmed on 12/05/20.
+//  Copyright © 2020 Amrendra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SukshiAssignment.
+FOUNDATION_EXPORT double SukshiAssignmentVersionNumber;
+
+//! Project version string for SukshiAssignment.
+FOUNDATION_EXPORT const unsigned char SukshiAssignmentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SukshiAssignment/PublicHeader.h>
+
+
+
+
+git remote add origin git@github.com:amrendradevios/Assignment.git
---
+++ SukshiTest/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SukshiTest
+//
+//  Created by Zoheb Ahmed on 12/05/20.
+//  Copyright © 2020 Amrendra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
